328855febb7bf8e012838cbfaebf493bc0c490c4
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-06 09:34:28 -0700

    add code to pass calc test, correctly adds 5 for each bread entered

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -5,10 +5,21 @@ namespace Bakery.Models
   public class Bread
   {
     public int BreadCount {get; set;}
+    public int BreadSubTotal {get; set;}
 
     public Bread(string count)
     {
       BreadCount = int.Parse(count);
+      BreadSubTotal = 0;
+    }
+
+    public int BreadCalc()
+    {
+      for (int i = 0; i < BreadCount; i++)
+      {
+        BreadSubTotal += 5;
+      }
+      return BreadSubTotal;
     }
   }
 }
