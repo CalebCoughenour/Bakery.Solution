d4954a3c9541f2153f954007b5b1314e4490218f
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-06 14:18:47 -0700

    add loop that will exit or restart program at end

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,26 +7,46 @@ public class Program
 {
   public static void Main()
   {
-    Console.WriteLine("Hello and welcome to Pierre's Bakery!");
-    Console.WriteLine("Our Bread Prices: $5 Each // Buy 2, get 1 Free!");
-    Console.WriteLine("Our Pastry Prices: $2 Each // Every 3rd Pastry ONLY $1!");
+    while(true)
+    {
+      Console.WriteLine("Hello and welcome to Pierre's Bakery!");
+      Console.WriteLine("Our Bread Prices: $5 Each // Buy 2, get 1 Free!");
+      Console.WriteLine("Our Pastry Prices: $2 Each // Every 3rd Pastry ONLY $1!");
+      
+      Console.WriteLine("How many bread loaves?");
+      string userBreadInput = Console.ReadLine();
+      Bread newBreadCount = new Bread(userBreadInput);
+      newBreadCount.BreadCalc();
+      newBreadCount.BreadSaleCalc();
 
-    Console.WriteLine("How many bread loafs?");
-    string userBreadInput = Console.ReadLine();
-    Bread newBreadCount = new Bread(userBreadInput);
-    newBreadCount.BreadCalc();
-    newBreadCount.BreadSaleCalc();
+      // while(true)
+      // {
+      //   Console.WriteLine("You want " + newBreadCount.BreadCount);
+      //   string userResponse = Console.ReadLine();
+      //   if(userResponse.ToUpper() == "Y")
+      //   {
+      //     break;
+      //   }
+      // }  
+      
 
-    
-    Console.WriteLine("How many pastries?");
-    string userPastryInput = Console.ReadLine();
-    Pastry newPastryCount = new Pastry(userPastryInput);
-    newPastryCount.PastryCalc();
-    newPastryCount.PastrySaleCalc();
+      
+      Console.WriteLine("How many pastries?");
+      string userPastryInput = Console.ReadLine();
+      Pastry newPastryCount = new Pastry(userPastryInput);
+      newPastryCount.PastryCalc();
+      newPastryCount.PastrySaleCalc();
 
-    int totalPrice = newBreadCount.BreadSubTotal + newPastryCount.PastrySubTotal;
+      int totalPrice = newBreadCount.BreadSubTotal + newPastryCount.PastrySubTotal;
 
-    Console.WriteLine("GRAND TOTAL: " + totalPrice);
-
+      Console.WriteLine("GRAND TOTAL: " + totalPrice);
+      Console.WriteLine("------------------------------------");
+      Console.WriteLine("Is this total okay? Enter 'Y' to EXIT, Enter 'N' to restart order");
+      string exitResponse = Console.ReadLine();
+      if (exitResponse.ToUpper() == "Y")
+      {
+        break;
+      }
+    }
   }
 }
